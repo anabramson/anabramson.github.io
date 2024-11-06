148a4d1c36bdb7f87a1b7e8f763cd3c6b4da2275
anabramson <ariana.abramson@gmail.com>
2024-11-06 17:20:40 +0100

    Clean start, remove sensitive data

---
+++ netlify/functions/getBooks.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET'
+  };
+
+  try {
+    const response = await fetch(
+      `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_NAME}`,
+      {
+        headers: {
+          'Authorization': `Bearer ${process.env.AIRTABLE_API_KEY}`
+        }
+      }
+    );
+    
+    const data = await response.json();
+    
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: 'Failed fetching books' })
+    };
+  }
+};
